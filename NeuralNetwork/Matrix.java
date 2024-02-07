e3404c49d6fb6a517b8952407837a7fe4a5eb1b5
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2024-02-07 14:20:00 -0600

    Complete Matrix class

--- NeuralNetwork/Matrix.java
+++ NeuralNetwork/Matrix.java
@@ -1,5 +1,8 @@
 package NeuralNetwork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Matrix extends BaseObject {
     private static final Logger<Matrix> LOGGER = new Logger<Matrix>(Matrix.class);
 
@@ -68,5 +71,173 @@ public class Matrix extends BaseObject {
         LOGGER.logMethod("isSameSize");
 
         return this.columns == m.columns && this.rows == m.rows;
+    }
+
+    /**
+     * Static method that will return a matrix where each point
+     * ret[r][c] equals a[r][c] - b[r][c]
+     * 
+     * @param a
+     * @param b
+     * @return A new Matrix
+     * @throws Exception
+     */
+    public static Matrix subtract(Matrix a, Matrix b) throws Exception {
+        LOGGER.logMethod("subtract");
+
+        if (!a.isSameSize(b)) {
+            throw new Exception("Matricies are not the same size.");
+        }
+        Matrix ret = new Matrix(a.rows, a.columns);
+
+        for (int r = 0; r < a.rows; r++) {
+            for (int c = 0; c < a.columns; c++) {
+                ret.data[r][c] = a.data[r][c] - b.data[r][c];
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Static method that will return a new Matrix where each point
+     * matrix[r][c] is moved to matrix[c][r].
+     * 
+     * @param a
+     * @return new Matrix[c][r]
+     */
+    public static Matrix transpose(Matrix a) {
+        LOGGER.logMethod("transpose");
+
+        Matrix ret = new Matrix(a.rows, a.columns);
+        for (int r = 0; r < a.rows; r++) {
+            for (int c = 0; c < a.columns; c++) {
+                ret.data[r][c] = a.data[c][r];
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Static method that will compute the dot product of 2 matricies.
+     * The resulting matrix will have a.rows number of rows and b.columns
+     * number of columns.
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    public static Matrix dotProduct(Matrix a, Matrix b) {
+        LOGGER.logMethod("dotProduct");
+
+        Matrix temp = new Matrix(a.rows, b.columns);
+        for (int i = 0; i < temp.rows; i++) {
+            for (int j = 0; j < temp.columns; j++) {
+                double sum = 0;
+                for (int k = 0; k < a.columns; k++) {
+                    sum += a.data[i][k] * b.data[k][j];
+                }
+                temp.data[i][j] = sum;
+            }
+        }
+
+        return temp;
+    }
+
+    /**
+     * This method will multiply each value[r][c] in the parameter to
+     * each corresponding value[r][c] in this.data.
+     * 
+     * @param a
+     */
+    public void multiply(Matrix a) {
+        LOGGER.logMethod("multiply");
+
+        for (int r = 0; r < a.rows; r++) {
+            for (int c = 0; c < a.columns; c++) {
+                this.data[r][c] *= a.data[r][c];
+            }
+        }
+    }
+
+    /**
+     * This is a direct scaling of the matrix. Each value[r][c] will be
+     * multiplied by the scalar (a).
+     * 
+     * @param a
+     */
+    public void multiply(double a) {
+        LOGGER.logMethod("multiply");
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                this.data[r][c] *= a;
+            }
+        }
+    }
+
+    /**
+     * This is will convert each value[r][c] into a number between 1
+     * and 0.
+     */
+    public void sigmoid() {
+        LOGGER.logMethod("sigmoid");
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                this.data[r][c] = 1 / (1 + Math.exp(-this.data[r][c]));
+            }
+        }
+    }
+
+    /**
+     * This will return a matrix of the derivative of {@link #sigmoid()}
+     * 
+     * @return
+     */
+    public Matrix dsigmoid() {
+        LOGGER.logMethod("dsigmoid");
+
+        Matrix temp = new Matrix(rows, columns);
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++)
+                temp.data[r][c] = this.data[r][c] * (1 - this.data[r][c]);
+        }
+        return temp;
+    }
+
+    /**
+     * This is a helper method to build start our matrix from an array.
+     * 
+     * @param x
+     * @return
+     */
+    public static Matrix fromArray(double[] x) {
+        LOGGER.logMethod("fromArray");
+
+        Matrix temp = new Matrix(x.length, 1);
+        for (int i = 0; i < x.length; i++) {
+            temp.data[i][0] = x[i];
+        }
+        return temp;
+
+    }
+
+    /**
+     * This is a helper method to convert our matrix into a List<Double>.
+     * @return
+     */
+    public List<Double> toArray() {
+        LOGGER.logMethod("toArray");
+
+        List<Double> temp = new ArrayList<Double>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                temp.add(data[i][j]);
+            }
+        }
+        return temp;
     }
 }
