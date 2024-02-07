4d02569b5a753ba2e649f762527c4dd000dd1289
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2024-02-06 20:08:18 -0600

    Create Logger.java

---
+++ NeuralNetwork/Logger.java
@@ -0,0 +1,23 @@
+package NeuralNetwork;
+
+import java.time.LocalDate;
+
+public class Logger<T extends BaseObject> {
+    Class<T> clazz;
+
+    public Logger(Class<T> clazz){
+        this.clazz = clazz;
+    }
+
+    public void log(String message){
+        System.out.println(String.format("%s [%s] %s",LocalDate.now().toString(),this.clazz.getName(), message));
+    }
+
+    public void logMethod(String name){
+        log(String.format("Start of method %s()", name));
+    }
+
+    public void error(String message, Throwable e){
+        System.out.println(String.format("%s [%s] ERROR %s %s",LocalDate.now().toString(),this.clazz.getName(), message, e));
+    }
+}
