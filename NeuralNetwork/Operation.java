4a9c38b5c1aeb8a8ca33db5280e062c607645e96
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2024-02-18 07:53:43 -0600

    Formatting

--- NeuralNetwork/Operation.java
+++ NeuralNetwork/Operation.java
@@ -3,61 +3,72 @@ package NeuralNetwork;
 public enum Operation {
     PLUS {
         @Override
-        public double getValue(){
+        public double getValue() {
             return 0.0;
         }
+
         @Override
         public int calc(final int i1, final int i2) {
             return i1 + i2;
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return "+";
         }
     },
     MINUS {
         @Override
-        public double getValue(){
+        public double getValue() {
             return 1.0;
         }
+
         @Override
         public int calc(final int i1, final int i2) {
             return i1 - i2;
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return "-";
         }
     },
     MULTIPLY {
         @Override
-        public double getValue(){
+        public double getValue() {
             return 2.0;
         }
+
         @Override
         public int calc(final int i1, final int i2) {
             return i1 * i2;
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return "*";
         }
     },
     DIVIDE {
         @Override
-        public double getValue(){
+        public double getValue() {
             return 3.0;
         }
+
         @Override
         public int calc(final int i1, final int i2) {
             return i1 / i2;
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return "/";
         }
     };
+
     public abstract double getValue();
+
     public abstract int calc(int i1, int i2);
+
     public abstract String toString();
 }
